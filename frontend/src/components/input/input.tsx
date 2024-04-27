ccff21ee52f80abf28aface243b371bda3a75ad6
rokartur <rokartur04@icloud.com>
2024-04-27 11:03:41 +0200

    close #4
    Took 2 minutes

--- frontend/src/components/input/input.tsx
+++ frontend/src/components/input/input.tsx
@@ -3,31 +3,20 @@ import { FC } from 'react'
 
 type InputTypes = {
 	title?: string
-	supportingText?: JSX.Element
-	icon?: JSX.Element
 	placeholder?: string
 	onChange?: (event: any) => void
 	value?: string
-	isBackground?: boolean
-	isPreview?: boolean
 }
 
-export const Input: FC<InputTypes> = ({ title, supportingText, icon, placeholder, onChange, value, isBackground, isPreview = false }) => (
-	<label className={`${styles.input}${isBackground ? ' ' + styles.inputBackground : ''}`}>
+export const Input: FC<InputTypes> = ({ title, placeholder, onChange, value }) => (
+	<label className={styles.input}>
 		{title ? <p>{title}</p> : null}
-		{supportingText ? <p>{supportingText}</p> : null}
-		{icon ? <span>{icon}</span> : null}
-		{isPreview ? (
-			<div className={`${styles.inputElement} ${icon ? styles.withIcon : styles.withOutIcon}`}>{value}</div>
-		) : (
-			<input
-				className={`${styles.inputElement} ${icon ? styles.withIcon : styles.withOutIcon}`}
-				type={'text'}
-				placeholder={placeholder}
-				onChange={onChange}
-				spellCheck={false}
-				value={value}
-			/>
-		)}
+		<input
+			type={'text'}
+			placeholder={placeholder}
+			onChange={onChange}
+			spellCheck={false}
+			value={value}
+		/>
 	</label>
 )
