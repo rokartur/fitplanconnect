83fd57a0a5c560fd209f8e461b85c3d243d45bdc
rokartur <rokartur04@icloud.com>
2024-05-09 21:22:44 +0200

    uh

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-const withNextra = require('nextra')({
-	theme: 'nextra-theme-docs',
-	themeConfig: './theme.config.tsx',
-	reactStrictMode: true,
-	distDir: 'dist',
-	output: 'export',
-})
-
-module.exports = withNextra()
