89c3cae592aa0a01b8e16bbb9570a9f144f79b30
rokartur <rokartur04@icloud.com>
2024-05-09 19:41:17 +0200

    refactor: update header

--- website/src/components/header/header.tsx
+++ website/src/components/header/header.tsx
@@ -185,9 +185,9 @@ export const Header = () => {
 										const response: OAuthResponse = await wretch('/api/oauth').get().json()
 										if (response.error) {
 											console.error(response.error)
-										} else if (response.url) {
-											window.location.href = response.url
 										}
+
+										window.location.href = response.url as string
 									}}
 								>
 									Login with GitHub
