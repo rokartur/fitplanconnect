5318fbba84cae837c6567e2058d7e544def37cd6
Pawel Polok <polok123.pp@gmail.com>
2024-05-14 20:04:24 +0200

    not found page placeholder for now

--- website/src/pages/notFound.tsx
+++ website/src/pages/notFound.tsx
@@ -1,12 +1,38 @@
+import styles from '@/styles/notFound.module.scss'
+import { AnimateWrapper } from '@/components/animateWrapper/animateWrapper'
+import { Container } from '@/components/container/container'
+import { Overlay } from '@/components/overlay/overlay'
+import { SEO } from '@/components/seo'
 import { useScrollTop } from '@/hooks/useScrollTop'
+import { Link } from 'react-router-dom'
+import { symlink } from 'fs'
+
+const metaData = {
+	title: 'Not Found',
+	description: 'Page not found',
+	path: '/404',
+}
 
 export default function NotFound() {
 	useScrollTop()
-  
+
 	return (
-		<div>
-			<h1>404</h1>
-			<p>Page not found</p>
-		</div>
+		<>
+			<SEO title={metaData.title} description={metaData.description} path={metaData.path} />
+
+			<Overlay>
+				<AnimateWrapper>
+					<Container>
+						<div className={styles.notFoundMainContainer}>
+							<h1>404</h1>
+							<p>Page not found</p>
+							<Link to={'/'}>
+								Back to home page
+							</Link>
+						</div>
+					</Container>
+				</AnimateWrapper>
+			</Overlay>
+    </>
 	)
 }
