f599bb88c5841d0c039fc20e2ba7d6361fe78990
rokartur <rokartur04@icloud.com>
2024-05-26 01:05:44 +0200

    chore: update vite config to use @vitejs/plugin-react-swc for faster refresh

--- website/src/hooks/useFetch.ts
+++
@@ -1,9 +0,0 @@
-import wretch from 'wretch'
-import useSWR from 'swr'
-
-const fetcher = (args: any) => wretch(args).get().json().then()
-
-export const useFetch = (url: string) => {
-	const { data, error, isLoading } = useSWR(url, fetcher, { revalidateOnFocus: false })
-	return { data, error, isLoading }
-}
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,6 @@
 import MillionLint from '@million/lint'
 import svgr from '@svgr/rollup'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 
