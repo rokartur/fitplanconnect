bddde7433ee55b48c9e42ffb31101b99742d9538
Pawel Polok <polok123.pp@gmail.com>
2024-06-25 19:29:11 +0200

    added overline and heading

--- website/src/components/features/features.tsx
+++ website/src/components/features/features.tsx
@@ -2,6 +2,18 @@ import styles from './features.module.scss'
 
 export default function Features() {
     return (
-        <section aria-label='features section' className={styles.features}></section>
+        <section aria-label='features section' className={styles.features}>
+            <div className={styles.container}>
+                <div className={styles.text}>
+                    <p className={styles.overline}>
+					    WHAT FITPLAN CONNECT OFFERS:
+				    </p>
+
+				    <h1 className={styles.heading}>
+					    Unlock your full potential with FitPlan Connect
+				    </h1>
+                </div>
+            </div>
+        </section>
     )
 }
