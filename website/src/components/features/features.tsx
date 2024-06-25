ec988a43f03be9fa7ea67725778a074f0f4a020c
Pawel Polok <polok123.pp@gmail.com>
2024-06-25 19:26:07 +0200

    created features component and styles

---
+++ website/src/components/features/features.tsx
@@ -0,0 +1,7 @@
+import styles from './features.module.scss'
+
+export default function Features() {
+    return (
+        <section aria-label='features section' className={styles.features}></section>
+    )
+}
