3504d9cebdb6ceefd86d9d42d75dd7b48f7ae692
Pawel Polok <polok123.pp@gmail.com>
2024-06-25 23:01:01 +0200

    created footer section and footer containers

---
+++ website/src/components/footer/footer.tsx
@@ -0,0 +1,24 @@
+import styles from './footer.module.scss'
+
+export default function Footer() {
+
+    return (
+        <section aria-label='footer section' className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.leadingContent}>
+                        <div className={styles.media}>
+
+                        </div>
+                        <div className={styles.links}>
+
+                        </div>
+                    </div>
+                    <div className={styles.icons}>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
