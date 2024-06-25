15d2a55ffcff759dc8b18a1d963324c980493f4c
Pawel Polok <polok123.pp@gmail.com>
2024-06-25 19:26:56 +0200

    created blog component and styles

---
+++ website/src/components/blog/blog.tsx
@@ -0,0 +1,9 @@
+import styles from './blog.module.scss'
+
+export default function Blog() {
+    return (
+        <section aria-label='blog section' className={styles.row}>
+
+        </section>
+    )
+}
