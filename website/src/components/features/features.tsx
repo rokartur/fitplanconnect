64c11457abaa3caab1ee4c8ba0b7984144f39222
Pawel Polok <polok123.pp@gmail.com>
2024-06-25 19:35:08 +0200

    added text to feature containers

--- website/src/components/features/features.tsx
+++ website/src/components/features/features.tsx
@@ -19,7 +19,8 @@ export default function Features() {
 
                         </div>
                         <div className={styles.text}>
-
+                            <h2 className={styles.title}>Custom workouts</h2>
+                            <p className={styles.description}>Create personalized workout plans tailored to your goals, whether it's building muscle, losing weight, or enhancing flexibility.</p>
                         </div>
                     </div>
                     <div className={styles.feature}>
@@ -27,7 +28,8 @@ export default function Features() {
 
                         </div>
                         <div className={styles.text}>
-
+                            <h2 className={styles.title}>Expert Trainers</h2>
+                            <p className={styles.description}>Access top trainers with experience in various fitness disciplines, from bodybuilding to yoga.</p>
                         </div>
                     </div>
                     <div className={styles.feature}>
@@ -35,7 +37,8 @@ export default function Features() {
 
                         </div>
                         <div className={styles.text}>
-
+                            <h2 className={styles.title}>Tracking and Progress</h2>
+                            <p className={styles.description}>Monitor your workouts, set goals, and track your progress with intuitive charts and data.</p>
                         </div>
                     </div>
                     <div className={styles.feature}>
@@ -43,7 +46,8 @@ export default function Features() {
 
                         </div>
                         <div className={styles.text}>
-
+                            <h2 className={styles.title}>Community Support</h2>
+                            <p className={styles.description}>Connect with like-minded individuals and share your fitness journey.</p>
                         </div>
                     </div>
                 </div>
