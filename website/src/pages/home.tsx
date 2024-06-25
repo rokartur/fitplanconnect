7a5cc47395f6a956fb416e908d539ae6d03b082d
Pawel Polok <polok123.pp@gmail.com>
2024-06-25 19:24:30 +0200

    added features and blog components

--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -1,6 +1,8 @@
 import { SEO } from '@/components/seo'
 import { useScrollTop } from '@/hooks/useScrollTop'
 import Hero from '@/components/hero/hero'
+import Blog from '@/components/blog/blog'
+import Features from '@/components/features/features'
 
 const metaData = {
 	title: 'Welcome to FitPlan Connect!',
@@ -15,6 +17,8 @@ export default function Home() {
 		<>
 			<SEO title={metaData.title} description={metaData.description} path={metaData.path} />
 			<Hero/>
+			<Features	/>
+			<Blog/>
     </>
 	)
 }
