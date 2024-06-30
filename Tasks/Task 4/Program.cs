d97e9611f523e3a4446e646fd2fe3bd42634056d
rokartur <rokartur@icloud.com>
2024-06-30 19:48:55 +0200

    feat(tasks): added 4 task

---
+++ Tasks/Task 4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_4
+{
+    public abstract class Device
+    {
+        protected string Name { get; set; }
+        protected string Model { get; set; }
+
+        protected Device(string name, string model)
+        {
+            Name = name;
+            Model = model;
+        }
+
+        public abstract void DisplayInfo();
+    }
+    
+    public class Laptop : Device
+    {
+        public Laptop(string name, string model) : base(name, model)
+        {
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Laptop: {Name}, Model: {Model}");
+        }
+    }
+
+    public class Desktop : Device
+    {
+        public Desktop(string name, string model) : base(name, model)
+        {
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Desktop: {Name}, Model: {Model}");
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main()
+        {
+            List<Device> devices = new List<Device>
+            {
+                new Laptop("Apple", "MacBook Pro"),
+                new Desktop("Lenovo", "ThinkCentre"),
+                new Laptop("Lenovo", "ThinkPad"),
+                new Laptop("Asus", "ZenBook"),
+                new Desktop("Dell", "OptiPlex")
+            };
+
+            foreach (var device in devices)
+            {
+                device.DisplayInfo();
+            }
+        }
+    }
+}
